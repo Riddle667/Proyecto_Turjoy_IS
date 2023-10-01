d56a985e31c4254ae33ffd55bfcd790b13b98977
GioxBRUSH <105091966+GioxBRUSH@users.noreply.github.com>
2023-10-01 14:27:47 -0300

    style: adds error messages in login
    adds error messages, specifically error when not filling in a required field and error when entering the wrong password or email address.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,39 +24,60 @@
               <br>
 
 
-              <form class="space-y-4 md:space-y-6" method = "POST" action="{{ route('index') }}">
+              <form class="space-y-4 md:space-y-6" method = "POST" action="#" novalidate>  
+                  @csrf
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-custom-50 dark:text-white font-mulish">Correo electrónico </label>
-                      <input type="email" name="email" id="email" class="py-4 font-mulish-light bg-green-custom border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ingresa tu correo electrónico" >
+                      <input type="email" name="email" id="email" class="py-4 font-mulish-light bg-green-custom border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ingresa tu correo electrónico" required >
                       @error('email')
-                        <p style = 'width=300px margin: 0 auto padding:20px border: 1px solid color: #ff8a80'>Debe ingresar su correo electrónico para iniciar sesión</p>
+                      <div class="whitespace-nowrap flex item-center max-w-xs ml-auto bg-gray-custom-150 text-xs text-red-custom-50 my-2 rounded-lg text-center p-2">
+                        <svg class="flex-shrink-0 w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        
+                        <p>{{ $message }}</p>
+
+                      </div>
                       @enderror
                   </div>
                   <div>
                       <label for="password" class="block mb-2 text-sm font-mulish-light text-gray-custom-50 dark:text-white">Contraseña</label>
-                      <input type="password" name="password" id="password" placeholder="Ingresa tu contraseña" class="py-4 font-mulish-light bg-green-custom border border-gray-300 text-gray-custom-50 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                      <input type="password" name="password" id="password" placeholder="Ingresa tu contraseña" class="py-4 font-mulish-light bg-green-custom border border-gray-300 text-gray-custom-50 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required>
+                      @error('password')
+                      <div class="flex item-center max-w-xs ml-auto bg-gray-custom-150 text-xs text-red-custom-50 my-2 rounded-lg text-center p-2">
+                        <svg class="flex-shrink-0 w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        
+                        <p>{{ $message }}</p>
+
+                      </div>
+                        
+                      @enderror
+
+                      @if(session('message'))
+                        <p class="mt-2 text-xs text-red-600 dark:text-red-custom-50">{{session('message') }}</p>
+                      @endif
                   </div>
                   <div class="flex items-center justify-between">
                       <div class="flex items-start">
-                         
+                          
                       </div>
                      
                   </div>
                   <button type="submit" class="py-4 bg-gradient-to-r from-blue-custom-50 to-blue-custom-100 font-mulish-light w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Iniciar sesión</button>
-               
-
+                
 
                 <br>
                 <br>
                 <br>
                   <div>
-                  <button type="submit" class="bg-gradient-to-r from-blue-custom-50 to-blue-custom-100 font-mulish-light px-6 py-0.5 text-s text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Atrás</button>
+                  <a href="#" type="button" class="bg-gradient-to-r from-blue-custom-50 to-blue-custom-100 font-mulish-light px-6 py-0.5 text-s text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Atrás</a>
                   </div>
               </form>
           </div>
       </div>
   </div>
 </section>
-
 
 @endsection
