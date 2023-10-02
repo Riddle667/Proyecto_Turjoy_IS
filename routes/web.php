c35d987fd653dfbb3cce2c5efb42bad7fc44e30f
Jairo Calcina <jairo.calcina@outlook.com>
2023-10-02 10:15:34 -0300

    fix: move route code from wrong file

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/', function () {
 
 Route::get('login',[LoginController::class, 'create'])->name('login');
 Route::post('login/store',[LoginController::class, 'store'])->name('login.store');
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/addroute', [RouteController::class, 'routeCheck'])->name('route.check');
+    Route::get('/result/routes', [RouteController::class, 'indexRoutes'])->name('routesAdd.index');
+});
