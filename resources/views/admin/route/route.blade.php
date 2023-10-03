a997dd4a6c3c3e76f9259f879c09f3ef2924ba24
NachoXx25 <105187185+NachoXx25@users.noreply.github.com>
2023-10-03 00:04:55 -0300

    style: changes in routes interface

--- resources/views/admin/route/route.blade.php
+++ resources/views/admin/route/route.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
 
-    @if ($validRows || $invalidRows || $duplicatedRows)
+@if ($validRows || $invalidRows || $duplicatedRows)
         <div class="flex flex-1 flex-col gap-2">
             <div class="my-8 mx-auto">
                 <a class="px-6 py-3 bg-green-500 hover:bg-green-700 transition-all text-white font-semibold rounded-lg"
-                    href="{{ route('dashboard') }}">Finalizar</a>
+                    href="#">Finalizar</a>
             </div>
 
             @if (count($validRows) > 0)
@@ -35,111 +35,57 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($validRows as $validRow)
-                                <tr class="bg-green-400 border-b dark:bg-gray-900 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
-                                        {{ $validRow['origen'] }}
-                                    </th>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $validRow['destino'] }}
-                                    </td>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $validRow['cantidad_de_asientos'] }}
-                                    </td>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $validRow['tarifa_base'] }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @endif
-            @if (count($invalidRows))
-                <h3 class="text-2xl text-black font-semibold uppercase text-center">
-                    Listado de viajes que presentaron errores
-                </h3>
-                <div class="relative overflow-x-auto sm:rounded-lg">
-                    <table class="w-1/2 mx-auto text-sm text-left text-gray-500 dark:text-gray-400 mb-2">
-                        <thead class="text-xs text-gray-700 uppercase bg-red-600 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Origen
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Destino
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Cantidad de asientos
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Tarifa base
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($invalidRows as $invalidRow)
-                                <tr class="bg-red-400 border-b dark:bg-gray-900 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
-                                        {{ $invalidRow['origen'] ? $invalidRow['origen'] : '---' }}
-                                    </th>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $invalidRow['destino'] ? $invalidRow['destino'] : '---' }}
-                                    </td>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $invalidRow['cantidad_de_asientos'] ? $invalidRow['cantidad_de_asientos'] : '---' }}
-                                    </td>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $invalidRow['tarifa_base'] ? $invalidRow['tarifa_base'] : '---' }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @endif
+                            @foreach ($orderRows as $orderRow)
+                                @if ($colors[$loop->index] == '0')
+                                    <tr class="bg-green-400 border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
+                                            {{ $orderRow['origen'] ? $orderRow['origen'] : '---' }}
+                                        </th>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ $orderRow['destino'] }}
+                                        </td>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ $orderRow['cantidad_de_asientos'] ? $orderRow['cantidad_de_asientos'] : '---' }}
+                                        </td>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            ${{ number_format($orderRow['tarifa_base'], 0, '.', '.') ?: '---' }}
+                                        </td>
+                                    </tr>
+                                @endif
+                                @if ($colors[$loop->index] == '1')
+                                    <tr class="bg-yellow-400 border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
+                                            {{ $orderRow['origen'] ? $orderRow['origen'] : '---' }}
+                                        </th>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ $orderRow['destino'] }}
+                                        </td>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ $orderRow['cantidad_de_asientos'] ? $orderRow['cantidad_de_asientos'] : '---' }}
+                                        </td>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            ${{ number_format($orderRow['tarifa_base'], 0, '.', '.') ?: '---' }} </td>
+                                    </tr>
+                                @endif
+                                @if ($colors[$loop->index] == '2')
+                                    <tr class="bg-red-400 border-b dark:bg-gray-900 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
+                                            {{ $orderRow['origen'] ? $orderRow['origen'] : '---' }}
+                                        </th>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ $orderRow['destino'] }}
+                                        </td>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ $orderRow['cantidad_de_asientos'] ? $orderRow['cantidad_de_asientos'] : '---' }}
+                                        </td>
+                                        <td class="px-6 py-4 text-white font-medium">
+                                            {{ is_numeric($orderRow['tarifa_base']) ? '$' . number_format((float) $orderRow['tarifa_base'], 0, '.', '.') : '---' }}
 
-            @if (count($duplicatedRows))
-                <h3 class="text-2xl text-black font-semibold uppercase text-center">
-                    Listado de viajes que se encuentran duplicados
-                </h3>
-                <div class="relative overflow-x-auto sm:rounded-lg">
-                    <table class="w-1/2 mx-auto text-sm text-left text-gray-500 dark:text-gray-400 mb-2">
-                        <thead class="text-xs text-gray-700 uppercase bg-amber-600 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Origen
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Destino
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Cantidad de asientos
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-white font-bold">
-                                    Tarifa base
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($duplicatedRows as $duplicatedRow)
-                                <tr class="bg-yellow-400 border-b dark:bg-gray-900 dark:border-gray-700">
-                                    <th scope="row"
-                                        class="px-6 py-4 font-medium text-white whitespace-nowrap dark:text-white">
-                                        {{ $duplicatedRow['origen'] }}
-                                    </th>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $duplicatedRow['destino'] }}
-                                    </td>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $duplicatedRow['cantidad_de_asientos'] }}
-                                    </td>
-                                    <td class="px-6 py-4 text-white font-medium">
-                                        {{ $duplicatedRow['tarifa_base'] }}
-                                    </td>
-                                </tr>
+                                        </td>
+                                @endif
                             @endforeach
                         </tbody>
                     </table>
@@ -147,51 +93,31 @@
             @endif
         </div>
     @else
-        <div class="flex flex-col flex-1 justify-center items-center my-6">
-            <div class="mb-12 mx-auto">
-                <a class="px-6 py-3 bg-red-500 hover:bg-red-700 transition-all text-white font-semibold rounded-lg"
+        <div class="flex flex-col flex-1 my-6 ">
+            <div class="mb-0">
+                <a class="px-6 py-3 bg-green-custom hover:bg-green-custom transition-all text-white font-semibold rounded-lg "
                     href="#">Volver</a>
             </div>
-            <form class="flex flex-col items-center w-1/2" action="{{ route('route.check') }}" method="POST"
+            <form class=" flex flex-col items-center w-1/2 -my-40 mr-auto" action="{{ route('route.check') }}" method="POST"
                 enctype="multipart/form-data">
                 @csrf
                 <div>
-                    <input type="file" name="document">
+                    <input type="file" name="document" class="rounded-lg flex mr-auto">
                     @error('document')
-                        <p class="bg-red-400 font-semibold my-4 text-lg text-center text-red-800 px-4 py-3 rounded-lg">
-                            {{ $message }}</p>
+                        <p class="bg-red-custom-50 font-semibold my-4 text-lg text-center text-red-custom-50 px-4 py-3 rouded-lg">
+                            {{ $message }}
+                        </p>
                     @enderror
+                    <button class=" p-2 bg-green-custom ml-100 text-white font-semibold rounded-lg" type="submit">
+                        ¿Estás seguro?
+                    </button>
                 </div>
 
-                <button class="lg:w-1/4 my-4 p-2 bg-green-400 rounded-sm text-white font-semibold" type="submit">
-                    Importar Viajes
-                </button>
             </form>
         </div>
     @endif
 
-    <div class="flex flex-col flex-1 my-6 ">
-        <div class="mb-0">
-            <a class="px-6 py-3 bg-green-custom hover:bg-green-custom transition-all text-white font-semibold rounded-lg "
-                href="#">Volver</a>
-        </div>
-        <form class=" flex flex-col items-center w-1/2 -my-40 mr-auto" action="{{ route('route.check') }}" method="POST"
-            enctype="multipart/form-data">
-            @csrf
-            <div>
-                <input type="file" name="document" class="rounded-lg flex mr-auto">
-                @error('document')
-                    <p class="bg-red-custom-50 font-semibold my-4 text-lg text-center text-red-custom-50 px-4 py-3 rouded-lg">
-                        {{ $message }}
-                    </p>
-                @enderror
-                <button class=" p-2 bg-green-custom ml-100 text-white font-semibold rounded-lg" type="submit">
-                    ¿Estás seguro?
-                </button>
-            </div>
 
-        </form>
-    </div>
 
     <table class="table-auto -my-16 mx-60 ">
         <tbody>
