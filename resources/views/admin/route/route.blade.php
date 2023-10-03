912fc67e1920e20e7674d2d6bad8690dbf95d8eb
NachoXx25 <105187185+NachoXx25@users.noreply.github.com>
2023-10-03 19:20:08 -0300

    style: adding some changes to the routes interface

--- resources/views/admin/route/route.blade.php
+++ resources/views/admin/route/route.blade.php
@@ -4,8 +4,44 @@
 
 @section('content')
     @if ($validRows || $invalidRows || $duplicatedRows)
+    <nav class="ml-60 bg-gray-custom border-gray-200 dark:bg-gray-900">
+        <div class="bg-gray-200 max-w-screen flex flex-wrap items-center justify-between mx-auto p-3">
+            <h2 class= "flex intems-center font-bold  ">Menú Sistema/Cargar Rutas de Viaje</h2>
+        </div>
+        </nav>
+        <div class="flex  bg-white text-gray-custom-150 font-bold text-left ml-8 text-xl">Menú del Sistema</div>
+            <div class="mr-auto">
+                <p class="px-9 mr-4 text-lg bg-green-custom py-1  rounded-sm inline-block">Cargar Rutas de Viaje</p>
+                <p class= "text-green-custom font-bold inline-block underline_green">Cargar Rutas de Viaje </p>
+                <hr class="border-black">
+                <a href="#" class="px-9 mr-4 text-lg  py-1 inline-block ">Buscar Reservas</a>
+                <br>
+                <a href="#" class="px-9 mr-4 text-lg py-1 inline-block" >Reporte de Reservas</a>
+                <br>
+            </div>
         <div>
-            <table class="table-auto -my-20 mx-60 ">
+
+
+        <div class="flex flex-col flex-1 my-6 ">
+            <form class="flex flex-col items-center w-1/2 -my-40 mx-6 mr-auto" action="{{ route('route.check') }}"
+                method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="flex items-center p-10 mt-6">
+                    <div class="ml-14 mb-10 ">
+                        <input type="file" name="document" class="rounded-lg inline-block ">
+                        @error('document')
+                            <p class="bg-red-custom-50 font-semibold my-2 mx-6 text-lg text-white px-4 py-3 rounded-lg text-right absolute right-0 top-full z-10">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                    <button class="py-2 ml-3 mb-10  bg-green-custom text-white font-semibold inline-block rounded-lg" type="submit">
+                        ¿Estás seguro?
+                    </button>
+                </div>
+            </form>
+        </div>
+            <table class="table-auto -my-20 mx-60  mt-2">
                 <tbody>
                     <tr>
                         <td class="border px-4 py-2 font-bold rounded">Simbología de colores y errores</td>
@@ -40,7 +76,7 @@
             </div>
         </div>
         @if (count($validRows) > 0)
-            <h3 class="text-2xl text-black font-semibold uppercase text-center">Listado de viajes</h3>
+            <h3 class="text-2xl text-black font-semibold uppercase text-center">Registro de tramos cargados en el sistema</h3>
             <div class="relative overflow-x-auto sm:rounded-lg mb-2">
                 <table class="w-1/2 mx-auto text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-green-600 dark:bg-gray-700 dark:text-gray-400">
@@ -118,31 +154,43 @@
         @endif
         </div>
     @else
-        <div class="flex flex-col flex-1 my-6 ">
-            <div class="mb-0">
-                <a class="px-6 py-3 bg-green-custom hover:bg-green-custom transition-all text-white font-semibold rounded-lg"
-                    href="#">Volver</a>
+        <nav class="ml-60 bg-gray-custom border-gray-200 dark:bg-gray-900">
+        <div class="bg-gray-200 max-w-screen flex flex-wrap items-center justify-between mx-auto p-3">
+            <h2 class= "flex intems-center font-bold  ">Menú Sistema/Cargar Rutas de Viaje</h2>
+        </div>
+        </nav>
+        <div class="flex  bg-white text-gray-custom-150 font-bold text-left ml-8 text-xl">Menú del Sistema</div>
+            <div class="mr-auto">
+                <p class="px-9 mr-4 text-lg bg-green-custom py-1  rounded-sm inline-block">Cargar Rutas de Viaje</p>
+                <p class= "text-green-custom font-bold inline-block underline_green">Cargar Rutas de Viaje </p>
+                <hr class="border-black">
+                <a href="#" class="px-9 mr-4 text-lg  py-1 inline-block ">Buscar Reservas</a>
+                <br>
+                <a href="#" class="px-9 mr-4 text-lg py-1 inline-block" >Reporte de Reservas</a>
+                <br>
             </div>
+        <div>
+        <a class="px-6 py-3 bg-green-custom hover:bg-green-custom transition-all text-white font-semibold rounded-lg"  href="#">Volver</a>
+        <div class="flex flex-col flex-1 my-6 ">
             <form class="flex flex-col items-center w-1/2 -my-40 mx-6 mr-auto" action="{{ route('route.check') }}"
                 method="POST" enctype="multipart/form-data">
                 @csrf
-                <div class="flex flex-col relative mx-6">
-                    <input type="file" name="document" class="rounded-lg flex">
-                    @error('document')
-                        <p
-                            class="bg-red-custom-50 font-semibold my-2 mx-6 text-lg text-white px-4 py-3 rounded-lg text-right absolute right-0 top-full z-10">
-                            {{ $message }}
-                        </p>
-                    @enderror
-                    <button class="p-2 bg-green-custom text-white font-semibold rounded-lg" type="submit">
+                <div class="flex items-center p-10">
+                    <div class="ml-14 mb-10 ">
+                        <input type="file" name="document" class="rounded-lg inline-block ">
+                        @error('document')
+                            <p class="bg-red-custom-50 font-semibold my-2 mx-6 text-lg text-white px-4 py-3 rounded-lg text-right absolute right-0 top-full z-10">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+                    <button class="py-2 ml-3 mb-10  bg-green-custom text-white font-semibold inline-block rounded-lg" type="submit">
                         ¿Estás seguro?
                     </button>
                 </div>
             </form>
         </div>
-
-        <div>
-            <table class="table-auto -my-25 mx-60 ">
+            <table class="table-auto -my-20 mx-60  mt-2">
                 <tbody>
                     <tr>
                         <td class="border px-4 py-2 font-bold rounded">Simbología de colores y errores</td>
@@ -171,4 +219,5 @@
             </table>
         </div>
     @endif
+
 @endsection
