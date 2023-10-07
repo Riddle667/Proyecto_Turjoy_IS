d58b2698a7666e108ce0ae038e43ade170d1eab8
Riddle667 <andrewfelipe5478@gmail.com>
2023-10-07 11:26:46 -0300

    refactor: added validation
    changes were made to email validation

--- app/Helpers/MyHelpers.php
+++ app/Helpers/MyHelpers.php
@@ -5,6 +5,7 @@ function makeMessages()
 
     $messages = [
         'email.required' => 'Debe ingresar su correo electrónico para iniciar sesión',
+        'email.email' => 'El formato del correo ingresado es incorrecto',
         'password.required' => 'Debe ingresar su contraseña para iniciar sesión',
         'document.required' => 'Debe seleccionar un archivo para cargar',
         'document.max' => 'El archivo no debe pesar más de 5MB',
