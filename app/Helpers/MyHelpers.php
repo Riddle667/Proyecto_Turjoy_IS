d748cb30ba7676bbc228001e3ac3a2022af8d30c
Riddle667 <andrewfelipe5478@gmail.com>
2023-10-07 21:53:26 -0300

    style: added a ten clock to the RES interface

--- app/Helpers/MyHelpers.php
+++ app/Helpers/MyHelpers.php
@@ -4,13 +4,12 @@ function makeMessages()
 {
 
     $messages = [
-        
+
         'email.required' => 'Debe ingresar su correo electrónico para iniciar sesión',
-        'email.email' => 'El formato del correo ingresado es incorrecto',
         'password.required' => 'Debe ingresar su contraseña para iniciar sesión',
         'document.required' => 'Debe seleccionar un archivo para cargar',
-        'document.max' => 'El archivo no debe pesar más de 5MB',
-        'document.mimes' => 'El archivo debe ser de tipo xlsx',
+        'document.max' => 'el tamaño máximo del archivo a cargar no puede superar los 5 megabytes',
+        'document.mimes' => 'el archivo seleccionado no es Excel con extensión .xlsx',
 
     ];
 
