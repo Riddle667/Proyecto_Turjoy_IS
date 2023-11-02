2df7d01099b29d04b267a851fc7565d10a023bc3
Jairo Calcina <jairo.calcina@outlook.com>
2023-11-02 14:35:24 -0300

    feat: add new function to count the occupied seats of a travel

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ticket;
 
 class TicketController extends Controller
 {
-    //
+    public static function getOccupiedSeats($routeId)
+    {
+        $occupiedSeats = 0;
+        Ticket::where('route_id', $routeId)->sum('seats', $occupiedSeats);
+
+        return $occupiedSeats;
+    }
 }
