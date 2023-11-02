6bb8286bba16c78e6ea8325e70694a757462b0ff
Vicente Alarcón <vicente.alarcon@alumnos.ucn.cl>
2023-11-02 15:25:56 -0300

    style: fix details on the main page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                     </div>
                 </div>
 
-                <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                <div class="absolute z-30 hidden space-x-3 -translate-x-1/2 bottom-5 left-1/2 md:flex">
                     <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
                         data-carousel-slide-to="0"></button>
                     <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div
-        class=" lg:max-w-xl p-10 space-y-8 sm:p-8 bg-gray-custom-150   rounded-lg shadow-xl dark:bg-gray-800 z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-56 border border-black">
+        class=" lg:max-w-xl max-h-screen p-10 space-y-8 sm:p-8 bg-gray-custom-150   rounded-lg shadow-xl dark:bg-gray-800 z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-56 border border-black">
         <h2 class="text-2xl font-bold text-gray-900 dark:text-white text-center ">
             Reserva de pasajes TurJoy
         </h2>
@@ -21,28 +21,29 @@
                 </select>
             </div>
             <div>
-                <label for="destino" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destino:</label>
-                <select name="destino" id=""
+                <label for="destinations"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destino:</label>
+                <select name="destinations" id="destinations"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option value="">Seleccione una opción</option>
                 </select>
             </div>
             <div>
-  <label for="asientos" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad de
+                <label for="asientos" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad de
                     asientos:</label>
                 <input type="number" name="asientos" id="" min="0" inputmode="numeric"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
 
             <div class="grid grid-cols-2 gap-4 mb-4">
-                <div class="flex items-center justify-center rounded  h-28 ">
+                <div class="flex items-center justify-center rounded  h-4 mt-2 md:h-6 lg:h-8">
                     <button type="submit" style="white-space: nowrap;"
                         class="px-1 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         <a href="#">Reservar Pasajes</a>
                     </button>
 
                 </div>
-                <div class="flex items-center justify-center rounded  h-28 ">
+                <div class="flex items-center justify-center rounded  h-4 mt-2 md:h-6 lg:h-8">
                     <img src="{{ asset('images/banner.png') }}" alt="bus" class=" ml-5  w-2/4">
                 </div>
                 {{-- <div class="flex items-center justify-start">
@@ -59,4 +60,3 @@
 @section('js')
     <script src="{{ asset('assets/js/index.js') }}"></script>
 @endsection
-
