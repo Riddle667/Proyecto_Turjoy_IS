37a94848c226211f58e9f91b5b8971c2244720fc
VicenteA18UCN <105085389+VicenteA18UCN@users.noreply.github.com>
2023-11-05 15:05:34 -0300

    feat: add validations in reservation form

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,94 +1,122 @@
-const selectOrigin = document.getElementById('origins');
-const selectDestination = document.getElementById('destinations');
-const inputDate = document.getElementById('date');
+const selectOrigin = document.getElementById("origins");
+const selectDestination = document.getElementById("destinations");
+const inputDate = document.getElementById("date");
+const seatsLabel = document.getElementById("seats");
+const seatsInput = document.getElementById("seatsInput");
 let availableSeats = 0;
 
+const toggleFields = (enable) => {
+    selectOrigin.disabled = !enable;
+    selectDestination.disabled = !enable;
+};
+
+const toggleSeats = (enable) => {
+    seatsInput.disabled = !enable;
+    if (seatsInput.enable) {
+        seatsInput.removeAttribute("placeholder"); // Eliminar el placeholder personalizado
+    } else {
+        seatsInput.setAttribute("placeholder", "Seleccione una opción");
+    }
+};
+
+const verifyFields = () => {
+    const date = inputDate.value;
+    console.log(date);
+    toggleSeats(false);
+    if (date) {
+        toggleFields(true);
+    } else {
+        toggleFields(false);
+    }
+};
+
 const addOriginsToSelect = (origins) => {
-    origins.forEach(origin => {
-        const option = document.createElement('option');
+    origins.forEach((origin) => {
+        const option = document.createElement("option");
         option.value = origin;
         option.text = origin;
         selectOrigin.appendChild(option);
     });
-}
+};
 
 const clearSelect = () => {
     while (selectDestination.firstChild) {
         selectDestination.removeChild(selectDestination.firstChild);
     }
-}
+};
 
 const addDestinationsToSelect = (destinations) => {
     // Limpiar los registros del campo destinos
     clearSelect();
     // Crear la opción por defecto
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.value = "";
     option.text = "Seleccione una opción";
     option.selected = true;
     selectDestination.appendChild(option);
-    destinations.forEach(destination => {
-        const option = document.createElement('option');
+    destinations.forEach((destination) => {
+        const option = document.createElement("option");
         option.value = destination;
         option.text = destination;
         selectDestination.appendChild(option);
     });
-}
+};
 
 const verifySeats = (e) => {
     const destination = selectDestination.value;
     const origin = selectOrigin.value;
     const date = inputDate.value;
+    if (selectDestination.value == "") toggleSeats(false);
+    else toggleSeats(true);
     if (date == "" || origin == "" || destination == "") return;
     fetch(`/get/seats/${origin}/${destination}/${date}`)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             // Manipula los datos recibidos aquí
             const seats = data.availableSeats;
             availableSeats = seats;
+            seatsLabel.textContent = seats + " asientos disponibles";
+            seatsInput.setAttribute("max", seats);
         })
-        .catch(error => {
-        })
-}
+        .catch((error) => {});
+};
 
 const loadDestinations = (e) => {
     const currentValue = selectOrigin.value;
+    seatsLabel.textContent = "";
     if (currentValue) {
         fetch(`/get/destinations/${currentValue}`)
-            .then(response => response.json())
-            .then(data => {
+            .then((response) => response.json())
+            .then((data) => {
                 // Manipula los datos recibidos aquí
                 const destinations = data.destinations;
                 addDestinationsToSelect(destinations);
             })
-            .catch(error => {
-
-            })
-    }
-    else {
+            .catch((error) => {});
+    } else {
         clearSelect();
-        const option = document.createElement('option');
+        const option = document.createElement("option");
         option.value = "";
         option.text = "Seleccione una opción";
         option.selected = true;
         selectDestination.appendChild(option);
     }
-}
+};
 
 const loadOrigins = (e) => {
-    fetch('/get/origins')
-        .then(response => response.json())
-        .then(data => {
+    fetch("/get/origins")
+        .then((response) => response.json())
+        .then((data) => {
             // Manipula los datos recibidos aquí
             const origins = data.origins;
             addOriginsToSelect(origins);
         })
-        .catch(error => {
-
-        })
+        .catch((error) => {});
 };
 
-document.addEventListener('DOMContentLoaded', loadOrigins);
-selectOrigin.addEventListener('change', loadDestinations);
-selectDestination.addEventListener('change', verifySeats);
-inputDate.addEventListener('change', verifySeats);
+document.addEventListener("DOMContentLoaded", loadOrigins);
+document.addEventListener("DOMContentLoaded", verifyFields);
+inputDate.addEventListener("change", verifyFields);
+selectOrigin.addEventListener("change", loadDestinations);
+selectDestination.addEventListener("change", verifySeats);
+inputDate.addEventListener("change", verifySeats);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,8 +30,8 @@
             </div>
             <div>
                 <label for="asientos" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad de
-                    asientos:</label>
-                <input type="number" name="asientos" id="" min="0" inputmode="numeric"
+                    asientos: <label id="seats" class="text-red-custom-100"></label></label>
+                <input type="number" name="asientos" id="seatsInput" min="0" inputmode="numeric"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
 
