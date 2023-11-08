e07cacedeedccf5bdd8cc41a349a514cc88a5776
Jairo Calcina <jairo.calcina@outlook.com>
2023-11-08 17:55:32 -0300

    feat: add functionalities to save a ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ticket;
+use App\Models\Route;
 
 class TicketController extends Controller
 {
@@ -13,5 +14,40 @@ class TicketController extends Controller
         Ticket::where('route_id', $routeId)->where('reservation_date', $date)->sum('seats', $occupiedSeats);
 
         return $occupiedSeats;
+    }
+
+    public function store(Request $request)
+    {
+        // Generar el numero de reserva
+        $code = generateReservationNumbers();
+        // Modificar request
+        $request->request->add(['code' => $code]);
+
+        // Validar
+        $makeMessages = makeMessages();
+        $this->validate($request, [
+            'seat' => ['required'],
+            'total' => ['required'],
+            'date' => ['date', 'required'],
+        ], $makeMessages);
+
+        //  Verificamos si la fecha ingresada es mayor a la fecha actual.
+        $invalidDate = validDate($request->date);
+        if ($invalidDate) {
+            return back()->with('message', 'La fecha debe ser igual o mayor a '.date('d-m-Y'));
+        }
+
+        // Crear la reserva
+        $ticket = Ticket::create([
+        'code' => $request->code,
+        'seat' => $request->seat,
+        'date' => $request->date,
+        'total' => $request->total,
+        'travel_id' => $request->routeId,
+        ]);
+
+        return redirect()->route('generate.pdf', [
+            'id' => $ticket->id,
+        ]);
     }
 }
