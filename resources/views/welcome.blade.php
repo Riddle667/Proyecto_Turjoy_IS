f6d6b87ca05d03d60a9d8a009face9311b54787b
NachoXx25 <105187185+NachoXx25@users.noreply.github.com>
2023-11-29 16:39:38 -0300

    style: add select to pick a payment method with 4 options

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,14 @@
 @if ($travelsAmount > 0 && !auth()->check())
     @section('content')
         <div
-            class=" lg:max-w-xl max-h-screen p-10 space-y-8 sm:p-8 xl:pr-12 xl:pl-12 bg-gray-custom-150   rounded-lg shadow-xl dark:bg-gray-800 z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-56 border border-black">
+            class=" lg:max-w-xl max-h-screen p-10 space-y-8 sm:p-8 xl:pr-12 xl:pl-12 bg-gray-custom-150   rounded-lg shadow-xl dark:bg-gray-800 z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-64 border border-black">
             <h2 class="sm:text-lg xl:text-2xl font-bold text-gray-900 dark:text-white text-center ">
                 Reserva de pasajes TurJoy
             </h2>
             <form id="formReservation" class="mt-8 space-y-6" action="{{ route('ticket.add') }}" method="POST">
                 @csrf
                 <div>
-                    <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha del
+                    <label for="date" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Fecha del
                         viaje:</label>
                     <input type="date" name="date" id="date"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -17,7 +17,7 @@
                 </div>
                 <div>
                     <label for="origins"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Origen:</label>
+                        class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Origen:</label>
                     <select name="origins" id="origins"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="">Seleccione una opción</option>
@@ -25,19 +25,38 @@
                 </div>
                 <div>
                     <label for="destinations"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destino:</label>
+                        class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Destino:</label>
                     <select name="destinations" id="destinations"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="">Seleccione una opción</option>
                     </select>
                 </div>
                 <div>
-                    <label for="seatsInput" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cantidad de
+                    <label for="seatsInput" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Cantidad de
                         asientos: <label id="seats" class="text-red-custom-100"></label></label>
                     <input type="number" name="seats" id="seatsInput" min="0" inputmode="numeric"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 </div>
-
+                <div>
+                    <label for="pay_method"
+                        class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Método de pago:</label>
+                    <select name="pay_method" id="pay_method"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="">Seleccione una opción</option>
+                        <option value="1">
+                            Transferencia
+                        </option>
+                        <option value="2">
+                            Tarjeta de débito
+                        </option>
+                        <option value="3">
+                            Tarjeta de crédito
+                        </option>
+                        <option value="4">
+                            Efectivo
+                        </option>
+                    </select>
+                </div>
                 <div class="grid gap-4 mb-4">
                     <div class="flex items-center justify-center rounded  h-4 mt-2 md:h-6 lg:h-8">
                         <button type="button" id="reservationButton" style="white-space: nowrap;"
